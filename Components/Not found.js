c6109f5ffeb79910546f61dd42f785a9ac42896c
Anjali-K-S25 <ksanjali661@gmail.com>
2025-04-18 15:55:56 +0530

    Create Not found.js

---
+++ Components/Not found.js
@@ -0,0 +1,10 @@
+import './NotFound.css';
+
+const NotFound = () => (
+  <div className="not-found">
+    <h1>Page Not Found</h1>
+    <p>Sorry, the page you requested does not exist.</p>
+  </div>
+);
+
+export default NotFound;
